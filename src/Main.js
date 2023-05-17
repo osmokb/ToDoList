059219e0a0a7c0573a5a81ac065004312b62c64e
오승민 <seungmin@oseungmin-ui-noteubug.local>
2023-05-17 15:52:39 +0900

    fix: 레이아웃 수정

--- src/Main.js
+++ src/Main.js
@@ -5,16 +5,18 @@ import "./Main.scss";
 const Main = () => {
   return (
     <div className="main">
-      <div className="title">SeungMin's To do list</div>
-      <div className="input-box">
-        <input
-          className="input-todo"
-          type="text"
-          placeholder="내용을 입력하세요"
-        ></input>
-        <button className="button">입력</button>
+      <div className="contain">
+        <div className="title">SeungMin's To do list</div>
+        <div className="input-box">
+          <input
+            className="input-todo"
+            type="text"
+            placeholder="내용을 입력하세요"
+          ></input>
+          <button className="button">입력</button>
+        </div>
+        <List />
       </div>
-      <List />
     </div>
   );
 };
